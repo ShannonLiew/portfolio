24f211e059cc991f0f4b26b241ab67f772b3c192
ShannonLiew <shannon.liew@thekoalacorp.com>
2020-01-22 03:13:50 -0500

    Updates

---
+++ precache-manifest.d247cbef5310cdac0a32f05f5f648226.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00bcf3e077afd565e46e1c9144d8fca",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fc0fd37cd9c4e72e04b1",
+    "url": "/portfolio/static/css/main.122313d4.chunk.css"
+  },
+  {
+    "revision": "edde9d8954b1285c6a46",
+    "url": "/portfolio/static/js/2.144046d4.chunk.js"
+  },
+  {
+    "revision": "fc0fd37cd9c4e72e04b1",
+    "url": "/portfolio/static/js/main.333e3911.chunk.js"
+  },
+  {
+    "revision": "0fb10d192b9f33362430",
+    "url": "/portfolio/static/js/runtime~main.9ce8b28c.js"
+  },
+  {
+    "revision": "183de736efe789394f26ed013b0b5cdd",
+    "url": "/portfolio/static/media/Calibre-Light.183de736.woff2"
+  },
+  {
+    "revision": "46b36969c7bb1d7ed4c8253e8f274788",
+    "url": "/portfolio/static/media/Calibre-Medium.46b36969.woff2"
+  },
+  {
+    "revision": "b63c62e591d0c8fbe2c8f009883346d5",
+    "url": "/portfolio/static/media/Calibre-Regular.b63c62e5.woff2"
+  },
+  {
+    "revision": "d8b856473a51d7eec2a9bccf2fbea54c",
+    "url": "/portfolio/static/media/Calibre-Semibold.d8b85647.woff2"
+  },
+  {
+    "revision": "99497ab65b5b8a7e5c18b7b3edde5c85",
+    "url": "/portfolio/static/media/SFMono-Medium.99497ab6.woff2"
+  },
+  {
+    "revision": "7faa66605357721067fe1b07d9103015",
+    "url": "/portfolio/static/media/SFMono-Regular.7faa6660.woff2"
+  },
+  {
+    "revision": "3e5f592c7c8964542aa8a52fd532282f",
+    "url": "/portfolio/static/media/sahil_verma_resume.3e5f592c.pdf"
+  },
+  {
+    "revision": "9c65c6385d240016039a343b56b41de3",
+    "url": "/portfolio/static/media/shannon.9c65c638.jpeg"
+  }
+]);
